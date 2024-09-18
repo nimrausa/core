2a6364b3ab6ab9f2214c0de4912b770597b395fd
nimrausa <nimraidris7@gmail.com>
2024-09-17 22:00:15 -0400

    commit

--- home/views.py
+++ home/views.py
@@ -4,5 +4,5 @@ from django.http import HttpResponse
 # Create your views here.
 
 def home(request):
-    return HttpResponse("This is the django application create, read, update, and, delete for the crud operations")
+    return HttpResponse("This is the django application create, read, update, and, delete for the crud operations and add auth function present in django")
                         
